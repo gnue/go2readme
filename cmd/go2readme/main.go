6c2f4fff29b1f8eee2cad355c46c5287ce56aed8
gnue <gnue@so-kukan.com>
2017-08-08 11:17:55 +0900

    ファイル出力に対応

--- cmd/go2readme/main.go
+++ cmd/go2readme/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"text/template"
@@ -13,6 +14,8 @@ import (
 //go:generate go-bindata -ignore=\.DS_Store assets/...
 
 var opts struct {
+	Write    bool   `short:"w" long:"write" description:"write to file"`
+	Output   string `short:"o" long:"output" description:"output file"`
 	Template string `short:"t" long:"template" description:"template file"`
 	Args     struct {
 		Dir string `positional-arg-name:"dir" default:"." description:"directory"`
@@ -23,6 +26,10 @@ func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		os.Exit(1)
+	}
+
+	if opts.Write && opts.Output == "" {
+		opts.Output = "README.md"
 	}
 
 	b, err := ReadFileOrAsset(opts.Template, "assets/README.md")
@@ -41,9 +48,25 @@ func main() {
 		Fatalln(err)
 	}
 
-	err = d.WriteTo(os.Stdout)
+	w := os.Stdout
+
+	if opts.Output != "" {
+		f, err := createFile(opts.Output, false)
+		if err != nil {
+			Fatalln(err)
+		}
+		defer f.Close()
+
+		w = f
+	}
+
+	err = d.WriteTo(w)
 	if err != nil {
 		Fatalln(err)
+	}
+
+	if opts.Output != "" {
+		fmt.Fprintf(os.Stderr, "written to '%s'\n", opts.Output)
 	}
 }
 
@@ -53,4 +76,27 @@ func ReadFileOrAsset(fname string, asset string) ([]byte, error) {
 	}
 
 	return Asset(asset)
+}
+
+func createFile(fname string, backup bool) (*os.File, error) {
+	if backup {
+		if _, err := os.Stat(fname); err == nil {
+			err := os.Rename(fname, fname+".bak")
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := f.Truncate(0); err != nil {
+		f.Close()
+		return nil, err
+	}
+
+	return f, err
 }
